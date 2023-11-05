c7fd5b86dc28d40aac509ba820bc7b1253a78925
lark1987 <bosworth210920@gmail.com>
2023-11-05 22:35:40 +0800

    1105-2235

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+
 const users = {};
+const goodPeople = {};
+const badPeople = {}
+
 
 io.on('connection', (socket) => {
 
@@ -28,16 +32,10 @@ io.on('connection', (socket) => {
     const myNamespace = io.of(`/${spaceId}`);
     myNamespace.on('connection', (roomSocket) => {
 
-      // 此區為連線 Namespace，處理線上人數。
+      // 此區處理 線上人數、離線清除。
 
       roomSocket.on('setUserName', (userName,userId) => {
         users[roomSocket.id]={'spaceId':spaceId,'userName':userName,'userId':userId}
-      })
-
-      roomSocket.on('disconnect',() => {
-        delete users[roomSocket.id]
-        const roomUsers = Object.values(users).filter(user => user.spaceId === spaceId);
-        myNamespace.emit('onlineUsers',roomUsers)
       })
 
       roomSocket.on('getOnlineUsers',() => {
@@ -46,22 +44,48 @@ io.on('connection', (socket) => {
         console.log('我是getOnlineUsers',users)
       })
 
+      roomSocket.on('disconnect',() => {
+        delete users[roomSocket.id]
+        delete goodPeople[roomSocket.id]
+        const roomUsers = Object.values(users).filter(user => user.spaceId === spaceId);
+        myNamespace.emit('onlineUsers',roomUsers)
+      })
 
-      // 此區為 產生角色按鈕 > 區分好人壞人的 room
+
+
+
+      // 此區為 產生角色按鈕 > 好人壞人 room
 
       roomSocket.on('getRoleButton', (newList) => {
         myNamespace.emit('roleButton',newList)
       });
 
-      roomSocket.on('joinGood', () => {
+      roomSocket.on('joinGood', (userName) => {
         roomSocket.join('goodPeople');
         myNamespace.to('goodPeople').emit('groupMessage','歡迎加入好人陣營');
+        goodPeople[roomSocket.id]={'spaceId':spaceId,'userName':userName}
       });
     
-      roomSocket.on('joinBad', () => {
+      roomSocket.on('joinBad', (userName) => {
         roomSocket.join('badPeople');
         myNamespace.to('badPeople').emit('groupMessage','歡迎加入壞人陣營');
+        badPeople[roomSocket.id]={'spaceId':spaceId,'userName':userName}
       });
+
+      // 進度到這裡，要把梅林抓出來
+
+      roomSocket.on('getBadPeopleList', () => {
+        myNamespace.to('badPeople').emit('badPeopleList',badPeople);
+      });
+
+      
+
+
+
+
+      
+
+      
 
     });
 
@@ -73,3 +97,15 @@ io.on('connection', (socket) => {
 server.listen(4000, () => {
   console.log('伺服器運行在 http://localhost:4000');
 });
+
+
+
+
+        // for (const socket of sockets) {
+        //   console.log(socket.id);
+        // }
+
+        // const sockets = await myNamespace.in("goodPeople").fetchSockets();
+        // const randomIndex = Math.floor(Math.random() * sockets.length);
+        // const randomSocket = sockets[randomIndex].id;
+        // console.log(randomSocket)
