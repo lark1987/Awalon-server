d0138a40d17576134c4a433cf5418b994164bc30
lark1987 <bosworth210920@gmail.com>
2023-11-16 18:18:32 +0800

    1116-1818

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://awalon.vercel.app'); 
+  // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// app.use(cors());
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
