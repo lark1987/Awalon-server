4ffc94b36ecbcc50ce9417e5b77cedd53da72e35
lark1987 <bosworth210920@gmail.com>
2023-11-16 18:38:52 +0800

    1116-1838

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const cors = require('cors');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Origin', ''); 
   res.header('Access-Control-Allow-Methods', '*');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Headers', '');
   next();
 });
 
