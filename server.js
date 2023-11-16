2318a4dd52c4866847040fe9d19adf6f95d43278
lark1987 <bosworth210920@gmail.com>
2023-11-16 17:04:59 +0800

    1116-1704

--- server.js
+++ server.js
@@ -2,23 +2,24 @@
 const http = require('http');
 const express = require('express');
 const { Server } = require('socket.io');
-const cors = require("cors");
+// const cors = require("cors");
 
-const corsOptions = {
-  origin: ["https://awalon.vercel.app"],
-  credentials: true
-};
+// const corsOptions = {
+//   origin: ["https://awalon.vercel.app"],
+//   credentials: true
+// };
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000","https://awalon.vercel.app"],
+    origin: "https://awalon.vercel.app",
+    allowedHeaders: ["awalon"],
     credentials: true
   }
 });
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
