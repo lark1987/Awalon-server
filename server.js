ed83bc8da7cf827a5415e176845491335bad0f0a
lark1987 <bosworth210920@gmail.com>
2023-11-16 18:32:31 +0800

    1116-1832

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const cors = require('cors');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://awalon.vercel.app'); 
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
